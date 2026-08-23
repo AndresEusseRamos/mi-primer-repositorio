ae7cfe64e7975fdf086660cdf2030b6ee513230f
Michael Eusse <maeusser@unadvirtual.edu.co>
2026-08-23 17:14:23 -0500

    Make time tracker interactive using inputs

---
+++ practica.py
@@ -0,0 +1,5 @@
+user_name = input("cual es tu nombre?")
+work_hours = int(input("horas de trabajo?")) 
+studying_hours = int(input("horas de estudio? ")) 
+time = work_hours + studying_hours
+print(f"hola {user_name} usas {time} horas en tu dia")
