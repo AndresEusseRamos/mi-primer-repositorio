a6efeed635fd72fc9d55853b623148ca57be0764
Michael Eusse <maeusser@unadvirtual.edu.co>
2026-08-23 23:17:07 -0500

    Add decision making logic using if/else conditions

--- practica.py
+++ practica.py
@@ -1,5 +1,10 @@
+perfect_studying_hours = 2
 user_name = input("cual es tu nombre?")
 work_hours = int(input("horas de trabajo?")) 
 studying_hours = int(input("horas de estudio? ")) 
 time = work_hours + studying_hours
 print(f"hola {user_name} usas {time} horas en tu dia")
+if studying_hours >= perfect_studying_hours:
+    print ("muy buen trabajo")
+else:
+    print ("hay que mejorar")
