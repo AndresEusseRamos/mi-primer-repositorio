1e162da8572bd94698223d9d2411d0867b9cadde
Michael Eusse <maeusser@unadvirtual.edu.co>
2026-08-25 22:29:44 -0500

    feat: implementa ciclo for para procesar lista de edades

--- practica.py
+++ practica.py
@@ -1,10 +1,8 @@
-perfect_studying_hours = 2
-user_name = input("cual es tu nombre?")
-work_hours = int(input("horas de trabajo?")) 
-studying_hours = int(input("horas de estudio? ")) 
-time = work_hours + studying_hours
-print(f"hola {user_name} usas {time} horas en tu dia")
-if studying_hours >= perfect_studying_hours:
-    print ("muy buen trabajo")
-else:
-    print ("hay que mejorar")
+edades = [13,16,11,10,0,18,25,42]
+for elemento in edades:
+    if elemento == 0:
+        print ("ERROR")
+    elif elemento < 18:
+        print ("Menor de edad")
+    else:
+        print ("Mayor de edad")
